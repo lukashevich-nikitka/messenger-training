7d4939c73e7e4d3b1e42131eae3ec2dbb6ebf325
Nikita <lukashevich-nikitka@bk.ru>
2022-09-09 18:06:25 +0300

    feat: add all necessary modules for future developing

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import store from './store/store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
